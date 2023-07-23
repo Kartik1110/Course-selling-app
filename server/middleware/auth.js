93de899486d378a9eb27cacaf6b87a13fc398519
Kartik Pal <kartik.p1110@gmail.com>
2023-07-24 00:02:46 +0530

    feat: changed server structure

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+/* This is the secret used to verify the jwt token */
+const SECRET_KEY = 's3cr3tk3333y';
+
+/* This middleware is used to verify the jwt token */
+const authenticateJwt = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+
+	if (authHeader) {
+		const token = authHeader.split(' ')[1];
+
+		jwt.verify(token, SECRET_KEY, (err, user) => {
+			if (err) {
+				return res.sendStatus(403);
+			}
+			req.user = user;
+			next();
+		});
+	} else {
+		res.sendStatus(401);
+	}
+};
+
+module.exports = { authenticateJwt, SECRET_KEY };
